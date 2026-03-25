dcd8b820643b3fd211a8d79c6bf0218e692f64eb
Shahmeerkhan137 <m.shahmeer2029@gmail.com>
2026-03-25 15:21:19 +0500

    Initial commit: Flask app with requirements.txt

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# This is the Flask application object
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! First Flaskk app "
+
+# Only run when executed directly
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
