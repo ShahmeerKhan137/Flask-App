660facb81dc08b67c8e886f6a8b5eca1b82f405d
Shahmeerkhan137 <m.shahmeer2029@gmail.com>
2026-04-01 17:25:57 +0500

    kubernetes, date, time

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! First Flaskk app, DevOps interns, testing, kubernetes, 1st April"
+    return "Hello! First Flaskk app, DevOps interns, testing, kubernetes, 1st April, 17:25"
 
 # Only run when executed directly
 if __name__ == "__main__":
